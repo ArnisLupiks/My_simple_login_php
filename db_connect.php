7885e50e9c6f01dbbb619d5cbf15a53957350abe
ArnisLupiks <arnislupiks@gmail.com>
2014-09-08 23:50:14 +0100

    added db_connect.php
    created bd connection and added login form to php

---
+++ db_connect.php
@@ -0,0 +1,21 @@
+<?php 
+	define("HOST", "localhost");
+	define("DBUSER", "root");
+	define("PASS", "");
+	define("DB", "testing");
+	
+	############## creating connection with mysql ##########
+	$conn = mysql_connect(HOST, DBUSER, PASS);
+	if (!$conn)
+	{
+		//if connection failed quit the script
+		die('Could not connect !<br /> Please contact the site\'s administrator.');
+	}
+	$db = mysql_select_db(DB);
+	if (!$db)
+	{
+		//cannot connect to the database
+		die('Could not connect to the database !<br /> Please contact the site\'s administrator');
+	}
+	
+?>
