0c6a6ddb8dc80fb1bed60ddbbb379164887b7c9d
ArnisLupiks <arnislupiks@gmail.com>
2014-09-10 00:10:03 +0100

    some mobile update
    updated css, added <meta> tag to assign mobile device screen size

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <html>
 <head>
 	<title>Travel With Me</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="style.css" type="text/css"/>
 	  <!-- Bootstrap -->
     <link href="bootstrap-3.2.0-dist/css/bootstrap.min.css" rel="stylesheet">
+	<link href="bootstrap-3.2.0-dist/css/bootstrap-theme.min.css" rel="stylesheet">
 </head>
 <body>
 	<div class="mainContainer">
@@ -12,6 +14,7 @@
 				<h2 class="from-signin-heading">Please Login</h2>
 				<input type="email" class="form-control" placeholder="Email" id="email" required autofocus />	
 				<input type="password" class="form-control" placeholder="Password" id="passwd" required />
+				<a class="forgot" href="#">Forgot your password?</a>
 				<button class="btn btn-lg btn-primary btn-block" type="submit"onClick="handleLogin()" >Sign in</button>
 			</form>	
 		</div>
--- login.php
+++ login.php
@@ -1,11 +1,19 @@
+<html>
+<head>
+<link rel="stylesheet" href="style.css" type="text/css"/>
+    <link href="bootstrap-3.2.0-dist/css/bootstrap.min.css" rel="stylesheet">
+
+</head>
 <form name="login-form" id="login-form" method="post" action="<?php echo $PHP_SELF; ?>"> 
   <fieldset> 
 
- <form class="form-signin" role="from"  method="POST" data-ajax="false">
+<form class="form-signin" role="from"  method="POST" data-ajax="false">
 				<h2 class="from-signin-heading">Please Login</h2>
 				<input type="email" class="form-control" placeholder="Email" id="email" required autofocus />	
 				<input type="password" class="form-control" placeholder="Password" id="passwd" required />
+				<a class="forgot" href="#">Forgot your password?</a>
 				<button class="btn btn-lg btn-primary btn-block" type="submit"onClick="handleLogin()" >Sign in</button>
 			</form>	
   </fieldset> 
 </form>
+</html>
