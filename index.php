7fa07aade1cf8763d48a91ae22d1f4215b501970
ArnisLupiks <arnislupiks@gmail.com>
2014-09-15 22:49:28 +0100

    some updates

--- index.php
+++ index.php
@@ -1,23 +1,34 @@
 <html>
 <head>
-	<title>Travel With Me</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Travel With Me</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="style.css" type="text/css"/>
 	  <!-- Bootstrap -->
-    <link href="bootstrap-3.2.0-dist/css/bootstrap.min.css" rel="stylesheet">
-	<link href="bootstrap-3.2.0-dist/css/bootstrap-theme.min.css" rel="stylesheet">
-</head>
+    <link href="bootstrap-3.2.0-dist/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+	<link href="bootstrap-3.2.0-dist/css/bootstrap-theme.min.css" rel="stylesheet"type="text/css" />
+	<!-- javascript -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+	<script src="bootstrap-3.2.0-dist/js/bootstrap.min.js"></script>
+	</head>
 <body>
-	<div class="mainContainer">
-		<div class="loginForm">
-			 <form class="form-signin" role="from"  method="POST" data-ajax="false">
-				<h2 class="from-signin-heading">Please Login</h2>
-				<input type="email" class="form-control" placeholder="Email" id="email" required autofocus />	
-				<input type="password" class="form-control" placeholder="Password" id="passwd" required />
-				<a class="forgot" href="#">Forgot your password?</a>
-				<button class="btn btn-lg btn-primary btn-block" type="submit"onClick="handleLogin()" >Sign in</button>
-			</form>	
+	<div class="container">
+		<div class="navbar navbar-default navbar-fixed-top navbar-collapse collapse" role="navigation">
+			<div class="container-fluid">
+			<ul class="nav navbar-nav navbar-left">
+				<h2>Travel With Me</h2>
+			</ul>
+			<ul class="nav navbar-nav navbar-right">
+				<button type="button" class="btn btn-link">Login</button>
+				<button class="btn btn-custom1" type="Login"onClick="handleLogin()" >Sign Up</button>
+			</ul>
+			</div>
 		</div>
+	</div>
+	<div id="content" class="content">
+	
+	</div>
+	<div id="footer" class="footer">
+	
 	</div>
 </body>
 </html>
