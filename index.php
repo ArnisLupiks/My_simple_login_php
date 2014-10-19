acf2b497a3c4da844b58d50232e3989fd7c4d509
ArnisLupiks <arnislupiks@gmail.com>
2014-10-20 00:32:51 +0100

    little done

--- index.php
+++ index.php
@@ -1,26 +1,27 @@
-<html>
+<html ng-app>
 <head>
 <title>Travel With Me</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="style.css" type="text/css"/>
+	<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
 	  <!-- Bootstrap -->
     <link href="bootstrap-3.2.0-dist/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
 	<link href="bootstrap-3.2.0-dist/css/bootstrap-theme.min.css" rel="stylesheet"type="text/css" />
 	<!-- javascript -->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 	<script src="bootstrap-3.2.0-dist/js/bootstrap.min.js"></script>
+
 	</head>
 <body>
+<header class="navbar navbar-default navbar-fixed-top navbar-inverse" ng-app>
 
-	<div class="container">
 
-		<div class="navbar navbar-fixed-top navbar-inverse">
 
 			<div class="container">
 
 				<a href ="#" class="navbar-brand">Travel Mate</a>
 
-				<button class = "navbar-toggle" data-toggle = "collapse" data-target = ".navHeaderCollapse">
+				<button class = "navbar-toggle" data-toggle = "collapse" data-target = ".navHeaderCollapse" ng-click="isCollapsed = !isCollapsed">
 					<span class = "icon-bar"></span>
 					<span class = "icon-bar"></span>
 					<span class = "icon-bar"></span>
@@ -43,10 +44,10 @@
 				</div>
 			</div>
 
-		</div>
 
-	</div>
 
+
+</header>
 	<div id="content" class="content">
 
 	</div>
